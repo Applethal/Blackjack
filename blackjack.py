4781554985539fe5c4c11c8a2ead6e67f911d8dd
Saad's grimoire of sorceries <114657050+Applethal@users.noreply.github.com>
2023-02-11 15:19:08 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -36,18 +36,6 @@ class blackjack:
                 self.O_value[0] = 1
                 self.O_value[1] = 11
 
-    def decision(self):
-        while self.value[0] < 22:
-            if self.value[-1] == 21:
-                print("Congratulations, no further instructions will be given, you already won!")
-            elif (self.value == [11, 11]) or (self.value == [10, 10]):
-                print("Unless you cannot, consider a double down, you will most likely hit 21.")
-            elif self.value[1] > 14:
-                print("Hit")
-                self.card = input("Enter the card that you have obtained now")
-                self.hand.append(self.card)
-                self.get_value()
-    
     def luck_computation(self):
         i = 0
         n = self.value[0]
@@ -59,17 +47,46 @@ class blackjack:
             distances = 21 - n - i
             distance_to_21.append(distances)
             distances = 0
-            distances = 11 - O_n - i 
+            distances = 11 - O_n - i
             O_distance_to_21.append(distances)
             i += 1
-        
+
         for luck in distance_to_21:
-            if 10 in self.hand or 10 in self.O_hand:
-                chance = (16 - (self.hand.count(10) + self.O_hand.count(10))) / (52 - (len(self.O_hand) +len(self.hand)))
-            elif self.hand.count(self.hand[0]) > ############ I NEED TO MODIFY THIS BUT I AM TIRED
-            
+            sum_chances = 0
+            if luck == 10:
+                chance = (16 -
+                          (self.hand.count(10) + self.O_hand.count(10))) / (52 - (len(self.O_hand) + len(self.hand)))
             else:
-                chance = (4 - distance_to_21.count(distance_to_21[luck]) ) / (52 - (len(self.O_hand) +len(self.hand)))
-            
-            
-            
+                chance = (4 - (self.hand.count(luck) +
+                               self.O_hand.count(luck))) / (52 - (len(self.O_hand) + len(self.hand)))
+
+            sum_chances += chance
+
+        for luck_d in O_distance_to_21:
+            sum_chances_d = 0
+            if luck_d == 10:
+                chance_d = (16 -
+                            (self.hand.count(10) + self.O_hand.count(10))) / (52 - (len(self.O_hand) + len(self.hand)))
+            else:
+                chance_d = (4 - (self.hand.count(luck_d) +
+                                 self.O_hand.count(luck_d))) / (52 - (len(self.O_hand) + len(self.hand)))
+
+            sum_chances_d += chance_d
+        return sum_chances_d, sum_chances
+
+    def decision(self):
+        while self.value[0] < 22:
+            if self.value[-1] == 21:
+                print("Congratulations, no further instructions will be given, you already won!")
+            elif (self.value == [11, 11]) or (self.value == [10, 10]):
+                print("Unless you cannot, consider a double down, you will most likely hit 21.")
+            elif self.value[1] > 12:
+                v1, v2 = self.luck_computation()
+
+                if v1 > v2:
+                    print("If you hit, chances are that you would not bust if you compare yours with the dealer")
+                    self.card = input("Enter the card that you have obtained now")
+                    self.hand.append(self.card)
+                    self.get_value()
+                else:
+                    print("Hitting is not advised in this case, Hold it!")
